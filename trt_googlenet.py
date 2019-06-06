0d7d09492d6efdcde2241ad5742a41b968c475f0
JK Jung <jkjung13@gmail.com>
2019-06-06 16:40:53 +0800

    Refactor trt_googlenet.py with utils/camera.py

--- trt_googlenet.py
+++ trt_googlenet.py
@@ -1,11 +1,7 @@
-"""camera_trt_googlenet.py
+"""trt_googlenet.py
 
 This script demonstrates how to do real-time image classification
 (inferencing) with Cython wrapped TensorRT optimized googlenet engine.
-
-Note a large portion of the code was copied from my previous example:
-'tegra-cam-caffe-threaded.py'
-https://gist.github.com/jkjung-avt/d408aaabebb5b0041c318f4518bd918f
 """
 
 
@@ -17,6 +13,7 @@ import threading
 import numpy as np
 import cv2
 from pytrt import PyTrtGooglenet
+from utils.camera import add_camera_args, Camera
 
 
 PIXEL_MEANS = np.array([[[104., 117., 123.]]], dtype=np.float32)
@@ -27,10 +24,6 @@ RESIZED_SHAPE = (224, 224)
 
 WINDOW_NAME = 'TrtGooglenetDemo'
 
-# The following 2 global variables are shared between threads
-THREAD_RUNNING = False
-IMG_HANDLE = None
-
 
 def parse_args():
     """Parse input arguments."""
@@ -38,63 +31,13 @@ def parse_args():
             'real-time image classification with TrtGooglenet '
             'on Jetson Nano')
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('--rtsp', dest='use_rtsp',
-                        help='use IP CAM (remember to also set --uri)',
-                        action='store_true')
-    parser.add_argument('--uri', dest='rtsp_uri',
-                        help='RTSP URI, e.g. rtsp://192.168.1.64:554',
-                        default=None, type=str)
-    parser.add_argument('--latency', dest='rtsp_latency',
-                        help='latency in ms for RTSP [200]',
-                        default=200, type=int)
-    parser.add_argument('--usb', dest='use_usb',
-                        help='use USB webcam (remember to also set --vid)',
-                        action='store_true')
-    parser.add_argument('--vid', dest='video_dev',
-                        help='device # of USB webcam (/dev/video?) [0]',
-                        default=0, type=int)
-    parser.add_argument('--width', dest='image_width',
-                        help='image width [640]',
-                        default=640, type=int)
-    parser.add_argument('--height', dest='image_height',
-                        help='image height [480]',
-                        default=480, type=int)
+    parser = add_camera_args(parser)
     parser.add_argument('--crop', dest='crop_center',
                         help='crop center square of image for Caffe '
                              'inferencing [False]',
                         action='store_true')
     args = parser.parse_args()
     return args
-
-
-def open_cam_rtsp(uri, width, height, latency):
-    gst_str = ('rtspsrc location={} latency={} ! '
-               'rtph264depay ! h264parse ! omxh264dec ! '
-               'nvvidconv ! '
-               'video/x-raw, width=(int){}, height=(int){}, '
-               'format=(string)BGRx ! '
-               'videoconvert ! appsink').format(uri, latency, width, height)
-    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
-
-
-def open_cam_usb(dev, width, height):
-    gst_str = ('v4l2src device=/dev/video{} ! '
-               'video/x-raw, width=(int){}, height=(int){} ! '
-               'videoconvert ! appsink').format(dev, width, height)
-    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
-
-
-def open_cam_onboard(width, height):
-    # On versions of L4T prior to 28.1, add 'flip-method=2' into gst_str
-    gst_str = ('nvcamerasrc ! '
-               'video/x-raw(memory:NVMM), '
-               'width=(int)2592, height=(int)1458, '
-               'format=(string)I420, framerate=(fraction)30/1 ! '
-               'nvvidconv ! '
-               'video/x-raw, width=(int){}, height=(int){}, '
-               'format=(string)BGRx ! '
-               'videoconvert ! appsink').format(width, height)
-    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
 
 
 def open_window(width, height):
@@ -105,18 +48,6 @@ def open_window(width, height):
     cv2.setWindowTitle(WINDOW_NAME,
                        'Camera TensorRT GoogLeNet Classification Demo '
                        'for Jetson Nano')
-
-
-def grab_img(cap):
-    """
-    This 'grab_img' function is designed to be run in the sub-thread.
-    Once started, this thread continues to grab new image and put it
-    into the global IMG_HANDLE, until THREAD_RUNNING is set to False.
-    """
-    global THREAD_RUNNING
-    global IMG_HANDLE
-    while THREAD_RUNNING:
-        _, IMG_HANDLE = cap.read()
 
 
 def show_top_preds(img, top_probs, top_labels):
@@ -177,16 +108,15 @@ def classify(img, net, labels, do_cropping):
     return (out_prob[top_inds], labels[top_inds])
 
 
-def loop_and_classify(net, labels, do_cropping):
+def loop_and_classify(cam, net, labels, do_cropping):
     """Continuously capture images from camera and do classification."""
-    global IMG_HANDLE
     show_help = True
     full_scrn = False
     help_text = '"Esc" to Quit, "H" for Help, "F" to Toggle Fullscreen'
     while True:
         if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
             break
-        img = IMG_HANDLE
+        img = cam.read()
         if img is not None:
             top_probs, top_labels = classify(img, net, labels, do_cropping)
             show_top_preds(img, top_probs, top_labels)
@@ -204,46 +134,26 @@ def loop_and_classify(net, labels, do_cropping):
 
 
 def main():
-    global THREAD_RUNNING
     args = parse_args()
     print('Called with args:')
     print(args)
 
     labels = np.loadtxt('googlenet/synset_words.txt', str, delimiter='\t')
 
-    # Open camera
-    if args.use_rtsp:
-        cap = open_cam_rtsp(args.rtsp_uri,
-                            args.image_width,
-                            args.image_height,
-                            args.rtsp_latency)
-    elif args.use_usb:
-        cap = open_cam_usb(args.video_dev,
-                           args.image_width,
-                           args.image_height)
-    else:  # By default, use the Jetson onboard camera
-        cap = open_cam_onboard(args.image_width,
-                               args.image_height)
-
-    if not cap.isOpened():
+    cam = Camera(args)
+    cam.open()
+    if not cam.is_opened:
         sys.exit('Failed to open camera!')
 
     # initialize the tensorrt googlenet engine
     net = PyTrtGooglenet(DEPLOY_ENGINE, ENGINE_SHAPE0, ENGINE_SHAPE1)
 
-    # Start the sub-thread, which is responsible for grabbing images
-    THREAD_RUNNING = True
-    th = threading.Thread(target=grab_img, args=(cap,))
-    th.start()
-
+    cam.start()
     open_window(args.image_width, args.image_height)
-    loop_and_classify(net, labels, args.crop_center)
+    loop_and_classify(cam, net, labels, args.crop_center)
 
-    # Terminate the sub-thread
-    THREAD_RUNNING = False
-    th.join()
-
-    cap.release()
+    cam.stop()
+    cam.release()
     cv2.destroyAllWindows()
 
 
---
+++ utils/camera.py
@@ -0,0 +1,203 @@
+"""camera.py
+
+This code implements the Camera class, which encapsulates code to
+handle IP CAM, USB webcam or the Jetson onboard camera.  In
+addition, this Camera class is further extended to take a video
+file or an image file as input.
+"""
+
+
+import logging
+import threading
+
+import numpy as np
+import cv2
+
+
+def add_camera_args(parser):
+    """Add parser augument for camera options."""
+    parser.add_argument('--file', dest='use_file',
+                        help='use a video file as input (remember to '
+                        'also set --filename)',
+                        action='store_true')
+    parser.add_argument('--image', dest='use_image',
+                        help='use an image file as input (remember to '
+                        'also set --filename)',
+                        action='store_true')
+    parser.add_argument('--filename', dest='filename',
+                        help='video file name, e.g. test.mp4',
+                        default=None, type=str)
+    parser.add_argument('--rtsp', dest='use_rtsp',
+                        help='use IP CAM (remember to also set --uri)',
+                        action='store_true')
+    parser.add_argument('--uri', dest='rtsp_uri',
+                        help='RTSP URI, e.g. rtsp://192.168.1.64:554',
+                        default=None, type=str)
+    parser.add_argument('--latency', dest='rtsp_latency',
+                        help='latency in ms for RTSP [200]',
+                        default=200, type=int)
+    parser.add_argument('--usb', dest='use_usb',
+                        help='use USB webcam (remember to also set --vid)',
+                        action='store_true')
+    parser.add_argument('--vid', dest='video_dev',
+                        help='device # of USB webcam (/dev/video?) [0]',
+                        default=0, type=int)
+    parser.add_argument('--width', dest='image_width',
+                        help='image width [640]',
+                        default=640, type=int)
+    parser.add_argument('--height', dest='image_height',
+                        help='image height [480]',
+                        default=480, type=int)
+    return parser
+
+
+def open_cam_rtsp(uri, width, height, latency):
+    """Open an RTSP URI (IP CAM)."""
+    gst_str = ('rtspsrc location={} latency={} ! '
+               'rtph264depay ! h264parse ! omxh264dec ! '
+               'nvvidconv ! '
+               'video/x-raw, width=(int){}, height=(int){}, '
+               'format=(string)BGRx ! videoconvert ! '
+               'appsink').format(uri, latency, width, height)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+
+def open_cam_usb(dev, width, height):
+    """Open a USB webcam.
+
+    We want to set width and height here, otherwise we could just do:
+        return cv2.VideoCapture(dev)
+    """
+    gst_str = ('v4l2src device=/dev/video{} ! '
+               'video/x-raw, width=(int){}, height=(int){} ! '
+               'videoconvert ! appsink').format(dev, width, height)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+
+def open_cam_onboard(width, height):
+    """Open the Jetson onboard camera.
+
+    On versions of L4T prior to 28.1, you might need to add
+    'flip-method=2' into gst_str.
+    """
+    gst_str = ('nvcamerasrc ! '
+               'video/x-raw(memory:NVMM), '
+               'width=(int)2592, height=(int)1458, '
+               'format=(string)I420, framerate=(fraction)30/1 ! '
+               'nvvidconv ! '
+               'video/x-raw, width=(int){}, height=(int){}, '
+               'format=(string)BGRx ! videoconvert ! '
+               'appsink').format(width, height)
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+
+def grab_img(cam):
+    """This 'grab_img' function is designed to be run in the sub-thread.
+    Once started, this thread continues to grab a new image and put it
+    into the global 'img_handle', until 'thread_running' is set to False.
+    """
+    while cam.thread_running:
+        _, cam.img_handle = cam.cap.read()
+        if cam.img_handle is None:
+            logging.warning('grab_img(): cap.read() returns None...')
+            break
+    cam.thread_running = False
+
+
+class Camera():
+    """Camera class which supports reading images from theses video sources:
+
+    1. Video file
+    2. Image (jpg, png, etc.) file, repeating indefinitely
+    3. RTSP (IP CAM)
+    4. USB webcam
+    5. Jetson onboard camera
+    """
+
+    def __init__(self, args):
+        self.args = args
+        self.is_opened = False
+        self.use_thread = False
+        self.thread_running = False
+        self.img_handle = None
+        self.img_width = 0
+        self.img_height = 0
+        self.cap = None
+        self.thread = None
+
+    def open(self):
+        """Open camera based on command line arguments."""
+        assert self.cap is None, 'Camera is already opened!'
+        args = self.args
+        if args.use_file:
+            self.cap = cv2.VideoCapture(args.filename)
+            # ignore image width/height settings here
+            self.use_thread = False
+        elif args.use_image:
+            self.cap = 'OK'
+            self.img_handle = cv2.imread(args.filename)
+            # ignore image width/height settings here
+            if self.img_handle is not None:
+                self.is_opened = True
+                self.img_height, self.img_width, _ = self.img_handle.shape
+            self.use_thread = False
+        elif args.use_rtsp:
+            self.cap = open_cam_rtsp(
+                args.rtsp_uri,
+                args.image_width,
+                args.image_height,
+                args.rtsp_latency
+            )
+            self.use_thread = True
+        elif args.use_usb:
+            self.cap = open_cam_usb(
+                args.video_dev,
+                args.image_width,
+                args.image_height
+            )
+            self.use_thread = True
+        else:  # by default, use the jetson onboard camera
+            self.cap = open_cam_onboard(
+                args.image_width,
+                args.image_height
+            )
+            self.use_thread = True
+        if self.cap != 'OK':
+            if self.cap.isOpened():
+                # Try to grab the 1st image and determine width and height
+                _, img = self.cap.read()
+                if img is not None:
+                    self.img_height, self.img_width, _ = img.shape
+                    self.is_opened = True
+
+    def start(self):
+        assert not self.thread_running
+        if self.use_thread:
+            self.thread_running = True
+            self.thread = threading.Thread(target=grab_img, args=(self,))
+            self.thread.start()
+
+    def stop(self):
+        self.thread_running = False
+        if self.use_thread:
+            self.thread.join()
+
+    def read(self):
+        if self.args.use_file:
+            _, img = self.cap.read()
+            if img is None:
+                #logging.warning('grab_img(): cap.read() returns None...')
+                # looping around
+                self.cap.release()
+                self.cap = cv2.VideoCapture(self.args.filename)
+                _, img = self.cap.read()
+            return img
+        elif self.args.use_image:
+            return np.copy(self.img_handle)
+        else:
+            return self.img_handle
+
+    def release(self):
+        assert not self.thread_running
+        if self.cap != 'OK':
+            self.cap.release()
