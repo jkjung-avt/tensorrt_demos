138db8c7a9ea59857b7b67e3870bffe54713e024
JK Jung <jkjung@gmail.com>
2019-12-27 16:53:04 +0800

    Add preliminary implementation of trt_yolov3.py (still work in progress)

---
+++ trt_yolov3.py
@@ -0,0 +1,92 @@
+"""trt_yolov3.py
+
+This script demonstrates how to do real-time object detection with
+TensorRT optimized YOLOv3 engine.
+"""
+
+
+import sys
+import time
+import argparse
+
+import cv2
+import pycuda.autoinit  # This is needed for initializing CUDA driver
+
+from utils.yolov3_classes import get_cls_dict
+from utils.yolov3 import TrtYOLOv3
+from utils.camera import add_camera_args, Camera
+from utils.display import open_window, set_display, show_fps
+from utils.visualization import BBoxVisualization
+
+
+WINDOW_NAME = 'TrtYOLOv3Demo'
+
+
+def parse_args():
+    """Parse input arguments."""
+    desc = ('Capture and display live camera video, while doing '
+            'real-time object detection with TensorRT optimized '
+            'YOLOv3 model on Jetson Nano')
+    parser = argparse.ArgumentParser(description=desc)
+    parser = add_camera_args(parser)
+    args = parser.parse_args()
+    return args
+
+
+def loop_and_detect(cam, trt_yolov3, conf_th, vis):
+    """Continuously capture images from camera and do object detection.
+
+    # Arguments
+      cam: the camera instance (video source).
+      trt_yolov3: the TRT YOLOv3 object detector instance.
+      conf_th: confidence/score threshold for object detection.
+      vis: for visualization.
+    """
+    full_scrn = False
+    fps = 0.0
+    tic = time.time()
+    while True:
+        if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
+            break
+        img = cam.read()
+        if img is not None:
+            boxes, confs, clss = trt_yolov3.detect(img, conf_th)
+            img = vis.draw_bboxes(img, boxes, confs, clss)
+            img = show_fps(img, fps)
+            cv2.imshow(WINDOW_NAME, img)
+            toc = time.time()
+            curr_fps = 1.0 / (toc - tic)
+            # calculate an exponentially decaying average of fps number
+            fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
+            tic = toc
+        key = cv2.waitKey(1)
+        if key == 27:  # ESC key: quit program
+            break
+        elif key == ord('F') or key == ord('f'):  # Toggle fullscreen
+            full_scrn = not full_scrn
+            set_display(WINDOW_NAME, full_scrn)
+
+
+def main():
+    args = parse_args()
+    cam = Camera(args)
+    cam.open()
+    if not cam.is_opened:
+        sys.exit('Failed to open camera!')
+
+    cls_dict = get_cls_dict('coco')
+    trt_yolov3 = TrtYOLOv3('yolov3')
+
+    cam.start()
+    open_window(WINDOW_NAME, args.image_width, args.image_height,
+                'Camera TensorRT YOLOv3 Demo')
+    vis = BBoxVisualization(cls_dict)
+    loop_and_detect(cam, trt_yolov3, conf_th=0.3, vis=vis)
+
+    cam.stop()
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
