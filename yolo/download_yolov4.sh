e136d0c8459fe8e94c41cec8d43a7e7499656950
JK Jung <jkjung13@gmail.com>
2020-08-04 22:18:31 +0800

    Correct the error as pointed out by issue #178

--- yolo/download_yolov4.sh
+++ yolo/download_yolov4.sh
@@ -12,7 +12,7 @@ cat yolov4.cfg | sed -e '2s/batch=64/batch=1/' | sed -e '7s/width=608/width=288/
 ln -sf yolov4.weights yolov4-288.weights
 cat yolov4.cfg | sed -e '2s/batch=64/batch=1/' | sed -e '7s/width=608/width=416/' | sed -e '8s/height=608/height=416/' > yolov4-416.cfg
 ln -sf yolov4.weights yolov4-416.weights
-cat yolov4.cfg | sed -e '2s/batch=64/batch=1/' > yolov4-416.cfg
+cat yolov4.cfg | sed -e '2s/batch=64/batch=1/' > yolov4-608.cfg
 ln -sf yolov4.weights yolov4-608.weights
 
 echo
