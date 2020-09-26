db4a5189a6c51926836c9a917cadba856af6a7f4
JK Jung <jkjung13@gmail.com>
2020-09-26 22:09:48 +0800

    Use the new build_engine() with config API for YOLO if TensorRT version >= 7

--- yolo/onnx_to_tensorrt.py
+++ yolo/onnx_to_tensorrt.py
@@ -59,68 +59,99 @@ import tensorrt as trt
 from plugins import get_input_wh, add_yolo_plugins
 
 
-EXPLICIT_BATCH = []
-if trt.__version__[0] >= '7':
-    EXPLICIT_BATCH.append(
-        1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
+MAX_BATCH_SIZE = 1
 
 
-def build_engine(onnx_path, category_num, do_int8, dla_core, verbose=False):
-    """Build a TensorRT engine from an ONNX file."""
-    model_name = onnx_path[:-5]
+def load_onnx(model_name):
+    """Read the ONNX file."""
+    onnx_path = '%s.onnx' % model_name
+    if not os.path.isfile(onnx_path):
+        print('ERROR: file (%s) not found!  You might want to run yolo_to_onnx.py first to generate it.' % onnx_path)
+        return None
+    else:
+        with open(onnx_path, 'rb') as f:
+            return f.read()
+
+
+def set_net_batch(network, batch_size):
+    """Set network input batch size.
+
+    The ONNX file might have been generated with a different batch size,
+    say, 64.
+    """
+    shape = list(network.get_input(0).shape)
+    shape[0] = batch_size
+    network.get_input(0).shape = shape
+    return network
+
+
+def build_engine(model_name, category_num, do_int8, dla_core, verbose=False):
+    """Build a TensorRT engine from ONNX using the older API."""
+    net_w, net_h = get_input_wh(model_name)
+
+    print('Loading the ONNX file...')
+    onnx_data = load_onnx(model_name)
+    if onnx_data is None:
+        return None
+
     TRT_LOGGER = trt.Logger(trt.Logger.VERBOSE) if verbose else trt.Logger()
+    EXPLICIT_BATCH = [] if trt.__version__[0] < '7' else \
+        [1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)]
     with trt.Builder(TRT_LOGGER) as builder, builder.create_network(*EXPLICIT_BATCH) as network, trt.OnnxParser(network, TRT_LOGGER) as parser:
-        # Parse model file
-        print('Loading ONNX file from path {}...'.format(onnx_path))
-        with open(onnx_path, 'rb') as model:
-            if not parser.parse(model.read()):
-                print('ERROR: Failed to parse the ONNX file.')
-                for error in range(parser.num_errors):
-                    print(parser.get_error(error))
-                return None
-        if trt.__version__[0] >= '7':
-            # The actual yolo*.onnx might have been generated with batch
-            # size 64.  We force batch size to 1 here.
-            shape = list(network.get_input(0).shape)
-            shape[0] = 1
-            network.get_input(0).shape = shape
+        if do_int8 and not builder.platform_has_fast_int8:
+            raise RuntimeError('INT8 not supported on this platform')
+        if not parser.parse(onnx_data):
+            print('ERROR: Failed to parse the ONNX file.')
+            for error in range(parser.num_errors):
+                print(parser.get_error(error))
+            return None
+        network = set_net_batch(network, MAX_BATCH_SIZE)
 
         print('Adding yolo_layer plugins...')
         network = add_yolo_plugins(
             network, model_name, category_num, TRT_LOGGER)
 
         print('Building an engine.  This would take a while...')
-        print('(Use "--verbose" to enable verbose logging.)')
-        if dla_core >= 0:
-            builder.max_batch_size = 1
-            config = builder.create_builder_config()
-            config.set_flag(trt.BuilderFlag.FP16)
-            config.set_flag(trt.BuilderFlag.INT8)
-            if not builder.platform_has_fast_int8:
-                raise ValueError('INT8 not supported on this platform')
-            from calibrator import YOLOEntropyCalibrator
-            net_w, net_h = get_input_wh(model_name)
-            config.int8_calibrator = YOLOEntropyCalibrator(
-                'calib_images', (net_h, net_w), 'calib_%s.bin' % model_name)
-            config.default_device_type = trt.DeviceType.DLA
-            config.DLA_core = dla_core
-            print('Using DLA core %d...' % dla_core)
-            engine = builder.build_engine(network, config)
-        else:
+        print('(Use "--verbose" or "-v" to enable verbose logging.)')
+        if trt.__version__[0] < '7':  # older API: build_cuda_engine()
+            if dla_core >= 0:
+                raise RuntimeError('DLA core not supported by old API')
+            builder.max_batch_size = MAX_BATCH_SIZE
             builder.max_workspace_size = 1 << 30
-            builder.max_batch_size = 1
             builder.fp16_mode = True  # alternative: builder.platform_has_fast_fp16
             if do_int8:
-                if not builder.platform_has_fast_int8:
-                    raise ValueError('INT8 not supported on this platform')
-                builder.int8_mode = True
                 from calibrator import YOLOEntropyCalibrator
-                net_w, net_h = get_input_wh(model_name)
+                builder.int8_mode = True
                 builder.int8_calibrator = YOLOEntropyCalibrator(
                     'calib_images', (net_h, net_w), 'calib_%s.bin' % model_name)
             engine = builder.build_cuda_engine(network)
+        else:  # new API: build_engine() with builder config
+            builder.max_batch_size = MAX_BATCH_SIZE
+            config = builder.create_builder_config()
+            config.max_workspace_size = 1 << 30
+            config.set_flag(trt.BuilderFlag.GPU_FALLBACK)
+            config.set_flag(trt.BuilderFlag.FP16)
+            if do_int8:
+                from calibrator import YOLOEntropyCalibrator
+                config.set_flag(trt.BuilderFlag.INT8)
+                config.int8_calibrator = YOLOEntropyCalibrator(
+                    'calib_images', (net_h, net_w), 'calib_%s.bin' % model_name)
+            profile = builder.create_optimization_profile()
+            profile.set_shape(
+                '000_net',                          # input tensor name
+                (MAX_BATCH_SIZE, 3, net_h, net_w),  # min shape
+                (MAX_BATCH_SIZE, 3, net_h, net_w),  # opt shape
+                (MAX_BATCH_SIZE, 3, net_h, net_w))  # max shape
+            config.add_optimization_profile(profile)
+            if dla_core >= 0:
+                config.default_device_type = trt.DeviceType.DLA
+                config.DLA_core = dla_core
+                config.set_flag(trt.BuilderFlag.STRICT_TYPES)
+                print('Using DLA core %d.' % dla_core)
+            engine = builder.build_engine(network, config)
 
-        print('Completed creating engine.')
+        if engine is not None:
+            print('Completed creating engine.')
         return engine
 
 
@@ -146,15 +177,15 @@ def main():
         help='id of DLA core for inference (0 ~ N-1)')
     args = parser.parse_args()
 
-    onnx_path = '%s.onnx' % args.model
-    if not os.path.isfile(onnx_path):
-        raise SystemExit('ERROR: file (%s) not found!  You might want to run yolo_to_onnx.py first to generate it.' % onnx_path)
-    engine_file_path = '%s.trt' % args.model
     engine = build_engine(
-        onnx_path, args.category_num, args.int8, args.dla_core, args.verbose)
-    with open(engine_file_path, 'wb') as f:
+        args.model, args.category_num, args.int8, args.dla_core, args.verbose)
+    if engine is None:
+        raise SystemExit('ERROR: failed to build the TensorRT engine!')
+
+    engine_path = '%s.trt' % args.model
+    with open(engine_path, 'wb') as f:
         f.write(engine.serialize())
-    print('Serialized the TensorRT engine to file: %s' % engine_file_path)
+    print('Serialized the TensorRT engine to file: %s' % engine_path)
 
 
 if __name__ == '__main__':
