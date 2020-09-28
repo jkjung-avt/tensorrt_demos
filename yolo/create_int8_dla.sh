8f694fff3328b8ae5c273c1df2e93cd93f0a6df5
JK Jung <jkjung13@gmail.com>
2020-09-28 17:27:10 +0800

    Add a script to facilitate creation of INT8 and DLA TensorRT engines

---
+++ yolo/create_int8_dla.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# I use this script to create INT8, DLA0 and DLA1 TensorRT engines for
+# various yolov3 and yolov4 models.
+
+set -e
+
+models="yolov3-tiny-416 yolov3-608 yolov3-spp-608 yolov4-tiny-416 yolov4-608"
+
+# make sure all needed files are present
+for m in ${models}; do
+  if [[ ! -f ${m}.cfg ]]; then
+    echo "ERROR: cannot find the file ${m}.cfg"
+    exit 1
+  fi
+  if [[ ! -f ${m}.onnx ]]; then
+    echo "ERROR: cannot find the file ${m}.onnx"
+    exit 1
+  fi
+done
+
+# create symbolic links
+for m in ${models}; do
+  m_head=${m%-*}
+  m_tail=${m##*-}
+  ln -sf ${m}.cfg  ${m_head}-int8-${m_tail}.cfg
+  ln -sf ${m}.onnx ${m_head}-int8-${m_tail}.onnx
+  ln -sf ${m}.cfg  ${m_head}-dla0-${m_tail}.cfg
+  ln -sf ${m}.onnx ${m_head}-dla0-${m_tail}.onnx
+  ln -sf ${m}.cfg  ${m_head}-dla1-${m_tail}.cfg
+  ln -sf ${m}.onnx ${m_head}-dla1-${m_tail}.onnx
+done
+
+# build TensorRT engines
+for m in ${models}; do
+  echo
+  echo === ${m} ===
+  m_head=${m%-*}
+  m_tail=${m##*-}
+  python3 onnx_to_tensorrt.py --int8 -m ${m_head}-int8-${m_tail}
+  python3 onnx_to_tensorrt.py --int8 --dla 0 -m ${m_head}-dla0-${m_tail}
+  python3 onnx_to_tensorrt.py --int8 --dla 1 -m ${m_head}-dla1-${m_tail}
+done
+
+echo
+echo "Done."
