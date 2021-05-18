b024850a45b65dfc4e479897b9ea90770d4674de
JK Jung <jkjung13@gmail.com>
2021-05-18 11:11:40 +0800

    Move main program loop to TrtMODNetRunner class

--- trt_modnet.py
+++ trt_modnet.py
@@ -8,6 +8,7 @@ TensorRT optimized MODNet engine.
 import os
 import time
 import argparse
+import subprocess
 
 import numpy as np
 import cv2
@@ -31,8 +32,116 @@ def parse_args():
     parser.add_argument(
         '--background', type=str, default='',
         help='background image or video file name [None]')
+    parser.add_argument(
+        '--create_video', type=str, default='',
+        help='create output video (either .ts or .mp4) [None]')
     args = parser.parse_args()
     return args
+
+
+def get_video_writer(name, width, height, fps=30):
+    """Get a VideoWriter object for saving output video.
+
+    This function tries to use Jetson's hardware H.264 encoder (omxh264enc)
+    if available, in which case the output video would be a MPEG-2 TS file.
+    Otherwise, it uses cv2's built-in encoding mechanism and saves a MP4
+    file.
+    """
+    gst_elements = str(subprocess.check_output('gst-inspect-1.0'))
+    if 'omxh264dec' in gst_elements:
+        filename = name + '.ts'  # Transport Stream
+        gst_str = ('appsrc ! videoconvert ! omxh264enc ! mpegtsmux ! '
+                   'filesink location=%s') % filename
+        return cv2.VideoWriter(
+            gst_str, cv2.CAP_GSTREAMER, 0, fps, (width, height))
+    else:
+        filename = name + '.mp4'  # MP4
+        return cv2.VideoWriter(
+            filename, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+
+
+def blend_img(img, bg, matte):
+    """Blend foreground and background using the 'matte'.
+
+    # Arguments
+        img: uint8 np.array of shape (H, W, 3), the foreground image
+        bg:  uint8 np.array of shape (H, W, 3), the background image
+        matte: float32 np.array of shape (H, W), values between 0.0 and 1.0
+    """
+    return (img * matte[..., np.newaxis] +
+            bg * (1 - matte[..., np.newaxis])).astype(np.uint8)
+
+
+class FpsCalculator():
+    """Helper class for calculating frames-per-second (FPS)."""
+
+    def __init__(self, decay_factor=0.95):
+        self.fps = 0.0
+        self.tic = time.time()
+        self.decay_factor = decay_factor
+
+    def update(self):
+        toc = time.time()
+        curr_fps = 1.0 / (toc - self.tic)
+        self.fps = curr_fps if self.fps == 0.0 else self.fps
+        self.fps = self.fps * self.decay_factor + \
+                   curr_fps * (1 - self.decay_factor)
+        self.tic = toc
+        return self.fps
+
+    def reset(self):
+        self.fps = 0.0
+
+
+class ScreenToggler():
+    """Helper class for toggling between non-fullscreen and fullscreen."""
+
+    def __init__(self):
+        self.full_scrn = False
+
+    def toggle(self):
+        self.full_scrn = not self.full_scrn
+        set_display(WINDOW_NAME, self.full_scrn)
+
+
+class TrtMODNetRunner():
+    """TrtMODNetRunner
+
+    # Arguments
+        modnet: TrtMODNet instance
+        cam: Camera object (for reading input images)
+        bggen: background generator (for reading background images)
+        writer: VideoWriter object (for saving output video)
+    """
+
+    def __init__(self, modnet, cam, bggen, writer=None):
+        self.modnet = modnet
+        sefl.cam = cam
+        self.bggen = bggen
+        self.writer = writer
+        open_window(
+            WINDOW_NAME, 'TensorRT MODNet Demo', cam.img_width, cam.img_height)
+
+    def run(self):
+        fps_calc = FpsCalculator()
+        scrn_tog = ScreenToggler()
+        while True:
+            if cv2.getWindowProperty(WINDOW_NAME, 0) < 0: break
+            img, bg = self.cam.read(), self.bggen.read()
+            if img is None: break
+            matte = self.modnet.infer(img)
+            matted_img = blend_img(img, bg, matte)
+            matted_img = show_fps(matted_img, fps)
+            cv2.imshow(WINDOW_NAME, matted_img)
+            fps = fps_calc.update()
+            key = cv2.waitKey(1)
+            if key == ord('F') or key == ord('f'):  # Toggle fullscreen
+                scrn_tog.toggle()
+            elif key == 27:                         # ESC key: quit
+                break
+
+    def __del__(self):
+        cv2.destroyAllWindows()
 
 
 def main():
@@ -41,38 +150,19 @@ def main():
     cam = Camera(args)
     if not cam.isOpened():
         raise SystemExit('ERROR: failed to open camera!')
-    bggen = Background(args.background, cam.img_width, cam.img_height)
+
+    writer = None
+    if args.create_video:
+        writer = get_video_writer(
+            args.create_video, cam.img_width, cam.img_height)
 
     modnet = TrtMODNet()
-    open_window(
-        WINDOW_NAME, 'TensorRT MODNet Demo', cam.img_width, cam.img_height)
+    bggen = Background(args.background, cam.img_width, cam.img_height)
 
-    full_scrn = False
-    fps = 0.0
-    tic = time.time()
-    while True:
-        if cv2.getWindowProperty(WINDOW_NAME, 0) < 0: break
-        img, bg = cam.read(), bggen.read()
-        if img is None: break
-        matte = modnet.infer(img)
-        matted_img = (
-            img * matte[..., np.newaxis] +
-            bg * (1 - matte[..., np.newaxis])).astype(np.uint8)
-        matted_img = show_fps(matted_img, fps)
-        cv2.imshow(WINDOW_NAME, matted_img)
-        toc = time.time()
-        curr_fps = 1.0 / (toc - tic)
-        fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
-        tic = toc
-        key = cv2.waitKey(1)
-        if key == 27:  # ESC key: quit program
-            break
-        elif key == ord('F') or key == ord('f'):  # Toggle fullscreen
-            full_scrn = not full_scrn
-            set_display(WINDOW_NAME, full_scrn)
+    runner = TrtMODNetRunner(modnet, cam, bggen, writer)
+    runner.run()
 
     cam.release()
-    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
